a65ec43354ab9cbedfdf19ca1200891345a32803
johngillard15 <johngillard15@gmail.com>
2021-06-24 22:04:51 -0400

    Added Pronic Number challenge

--- edabit Challenges/PronicNumber/src/com/company/Main.java
+++ edabit Challenges/PronicNumber/src/com/company/Main.java
@@ -1,14 +1,13 @@
 package com.company;
 
-import java.io.*;
-import java.util.*;
-import java.math.*;
-
 /**
- * Pronic Number
+ * <h1>Pronic Number</h1>
  *
- * A pronic number (or otherwise called as heteromecic) is a number which is a product of two consecutive integers, that is, a number of the form n(n + 1).
- * Create a function that determines whether a number is pronic or not.
+ * <a href="https://edabit.com/challenge/mMc9D6vB4iPiiAK7k">https://edabit.com/challenge/mMc9D6vB4iPiiAK7k</a>
+ *
+ * <p>A pronic number (or otherwise called as heteromecic) is a number which is a product of two consecutive integers,
+ * that is, a number of the form n(n + 1).
+ * Create a function that determines whether a number is pronic or not.</p>
  *
  * @since 24/6/2021
  * @author John Gillard
@@ -28,7 +27,7 @@ public class Main {
         System.out.println(isHeteromecic(0)); // ➞ true
         // 0 * (0 + 1) = 0 * 1 = 0
 
-        (isHeteromecic(2)); // ➞ true
+        System.out.println(isHeteromecic(2)); // ➞ true
         // 1 * (1 + 1) = 1 * 2 = 2
 
         System.out.println(isHeteromecic(7)); // ➞ false
